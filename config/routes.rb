d9fd0698068fa0c5cfd269f64719a9622450e7cf
smottu <106876134+smottu@users.noreply.github.com>
2023-02-19 05:38:06 +0000

    Kinou

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   root to: "homes#top"
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
